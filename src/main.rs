906e8241d6a50ffa0be0dc7016e1d76d980540fb
Vigintillionn <yarne.ramakers@gmail.com>
2023-11-14 23:35:12 +0100

    Create repl environment

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-use std::fs;
 mod parser;
 mod runtime;
 
 fn main() {
-    // Read a file
-    let src = fs::read_to_string("src/test.txt").unwrap();
-    // Parse the file
-    let tokens = parser::lexer::tokenize(&src);
-    let mut parser = parser::Parser::new(&tokens);
-    let ast = parser.produce_ast();
+    repl();
+}
 
-    let result = runtime::evaluate(ast);
-    println!("{:?}", result);
+fn repl() {
+    // Create a repl in the terminal
+    println!("Repl version 0.1.0");
+
+    loop {
+        // Read a line
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        // Parse the line
+        let tokens = parser::lexer::tokenize(&input);
+        let mut parser = parser::Parser::new(&tokens);
+        let ast = parser.produce_ast();
+
+        // Evaluate the line
+        let result = runtime::evaluate(ast);
+        println!("{:?}", result);
+    }
 }
