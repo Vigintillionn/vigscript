5346325c9a6bd02539f2749f559803c703dfdb86
Vigintillionn <yarne.ramakers@gmail.com>
2023-11-14 23:43:24 +0100

    Added option to run a file or to use the repl in the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,41 @@
+use std::fs;
 mod parser;
 mod runtime;
 
 fn main() {
-    repl();
+    // check if there is a file to run in the arguments
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 1 {
+        let filename = &args[1];
+        println!("{}", &filename);
+        let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+        interpret(&contents);
+    } else {
+        repl();
+    }
 }
 
 fn repl() {
     // Create a repl in the terminal
     println!("Repl version 0.1.0");
-
+    let mut input = String::new();
     loop {
-        // Read a line
-        let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
-
-        // Parse the line
-        let tokens = parser::lexer::tokenize(&input);
-        let mut parser = parser::Parser::new(&tokens);
-        let ast = parser.produce_ast();
-
-        // Evaluate the line
-        let result = runtime::evaluate(ast);
-        println!("{:?}", result);
+        if input == "exit\n" {
+            break;
+        }
+        interpret(&input);
+        input.clear();
     }
+}
+
+fn interpret(input: &str) {
+    let tokens = parser::lexer::tokenize(&input);
+    let mut parser = parser::Parser::new(&tokens);
+    let ast = parser.produce_ast();
+
+    let result = runtime::evaluate(ast);
+    println!("{:?}", result);
 }
